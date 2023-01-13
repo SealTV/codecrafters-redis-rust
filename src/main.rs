7c8540ae68330db0e7a755680b7be7cf1ecb7657
Valeriy Tetevin <seal.matob@gmail.com>
2023-01-13 21:36:28 +0300

    pass 2st stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::net::TcpListener;
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -6,11 +8,34 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                println!("New connection: {}", _stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(_stream)
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            let msg = "PONG";
+            let data = msg.as_bytes();
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
 }
